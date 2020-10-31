6fac8bda5dac978c4de887f90a8ca6d3af523942
Al <acordero3214@hotmail.com>
2020-10-30 22:29:01 -0500

    finish step 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,15 @@
 ## Part 1: Test it with SQL
+    employer: VARCHAR(255)
+    id: int
+    name: VARCHAR(255)
+    skills: VARCHAR(255)
 
 ## Part 2: Test it with SQL
+    SELECT name
+    FROM employer
+    WHERE location = 'St. Louis City';
 
 ## Part 3: Test it with SQL
+    DROP TABLE jobs;
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,32 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    private String location;
 
+    @OneToMany(mappedBy = "employer")
+    private List<Job> jobs = new ArrayList<> ();
+
+    public Employer(@NotBlank String location) {
+        this.location = location;
+    }
+
+    public Employer () {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
